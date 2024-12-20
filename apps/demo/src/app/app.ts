0a4e05d0bd0b91d5c96c88362b599570a8b4ab73
Dustin <dustingriffith@northwesternmutual.com>
2024-12-20 18:53:11 -0500

    yay

--- apps/demo/src/app/app.ts
+++ apps/demo/src/app/app.ts
@@ -1,3 +1,4 @@
 import { LitElement } from 'lit';
+import css from './index.css' assert { type: 'css' };
 
 export class App extends LitElement {}
---
+++ packages/core/dist/index.js
@@ -0,0 +1,4 @@
+// src/index.ts
+console.log("Hello via Bun!");
+
+//# debugId=521C4B34AAF70ABA64756E2164756E21
