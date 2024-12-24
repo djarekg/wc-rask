de514d39d7b071a01be5d546c83d0fdc3ea166f2
Dustin Griffith <dustingriffith@outlook.com>
2024-12-24 12:43:39 -0500

    works

--- packages/components/src/notification/notification.ts
+++ packages/components/src/notification/notification.ts
@@ -1,9 +1,7 @@
-import { type CSSResult, LitElement, type TemplateResult, html } from 'lit';
-import css from './notication.css' with { type: 'css' };
+import { LitElement, type TemplateResult, html } from 'lit';
+import './notication.css';
 
 export class Notification extends LitElement {
-  static override styles: CSSResult = css;
-
   override render(): TemplateResult {
     return html`<div><slot></slot></div>`;
   }
